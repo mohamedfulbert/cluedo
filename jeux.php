90404d938aa089c8a7c3caeb82754f0e92292112
mohamedfulbert <mohamed.khemiri@lyceefulbert.fr>
2024-12-13 17:47:20 +0100

    requete personnage

--- jeux.php
+++ jeux.php
@@ -1,3 +1,16 @@
+<?php
+$bdd_fichier = 'cluedo.db';	
+$sqlite = new SQLite3($bdd_fichier);
+$sql = 'SELECT id_personnage, nom_personnage, couleur ';
+	$sql .= 'FROM personnages';
+    $requete = $sqlite -> prepare($sql);
+    //echo $sql;
+    //:echo $sqlite ->lastErrorMsg();
+    $result = $requete -> execute();
+    while($adj = $result -> fetchArray(SQLITE3_ASSOC)) {
+		echo '<li>'.$adj['nom_personnage']." (id : {$adj['id_personnage']})</li>";
+	}
+    ?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -8,5 +21,6 @@
 <body>
     <h1>voici les lieux du crimes</h1>
     <img src="cluedoscreen.png" alt="voici les lieux du crime">
+        <li><a href="cluedo.php">retour a l'acceuil</a></li>
 </body>
 </html>
